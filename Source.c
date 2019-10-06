d1f1509d6ff7c36640d8d63413e34da168279a94
Adam <adam.rybansky@centrum.sk>
2019-10-06 10:18:38 +0200

    merge je efektivnejsi

--- Source.c
+++ Source.c
@@ -1,192 +1,122 @@
 #include <stdio.h>
 
 
-typedef struct hlavicka {					//hlavicka je na zaciatku volnej pamate, obsahuje smernik na prvy volny blok, a smernik na koniec pamate
+typedef struct head {					//hlavicka je na zaciatku volnej pamate, obsahuje smernik na prvy volny blok, a smernik na koniec pamate
 	struct block* first_free;
 	void* end;
-}hlavicka;
+}head;
 
-typedef struct block {
+typedef struct block {			//hlavicka kazdeho bloku obsahuje velkost bloku a smernik na nasledujuci blok
 	unsigned int size;				//ak je blok volny, pripocitame k size 1 (inak je size vzdy parne)
-	struct block* next;
+	struct block* next;			//ak blok obsadime, smernik na dalsi uz potrebovat nebudeme, takze data mozeme vpisovat aj sem
 }block;
 
-hlavicka* start = NULL;				//tento smernik bude vzdy ukazovat na prvy volny blok
+head* start = NULL;				//globalny smernik bude ukazovat na hlavicku pamate
 
-void memory_init(void* ptr, unsigned int size) {
-	start = ptr;
+void memory_init(void* memory, unsigned int memory_size) {
+	start = memory;
 	start->first_free = start + 1;
-	start->first_free->size = size - sizeof(hlavicka) - sizeof(int) + 1;
+	start->first_free->size = memory_size - sizeof(head) - sizeof(int) + 1;
 	start->first_free->next = NULL;
-	start->end = (char*) start + size;
+	start->end = (char*) start + memory_size;
 }
 
 void* memory_alloc(unsigned int required_size) {					
-	block* aktualny = start->first_free , *prev = NULL;
-	//printf("pustila sa funkcia memory alloc a hlada miesto pre blok velkosti %d\n", required_size);
-	if (required_size % 8 != 0){
+	block* current = start->first_free , *prev = NULL;
+	if (required_size % 8 != 0){		//velkost za vzdy zaukruhli smerom hore na najblizsi nasobok 8
 		required_size += (8 - required_size % 8);
 	}
-	while (aktualny != NULL) {
-		if (aktualny->size % 2 == 1) {						
-			if ((aktualny->size - 1 > required_size + sizeof(int)) && ((char*)aktualny + sizeof(int) + required_size + sizeof(block) < start->end)) {
-				/*nasli sme blok dost velky na to aby sme ho mohli rozdelit a zobrat si iba cast z neho*/
-				//printf("viac miesta ako treba  (%d), rozdeli sa\n\n\n", aktualny->size);
+	while (current != NULL) {		//prehladavame volne bloky, kym nenajdeme taky co vyhovuje
+		if ((current->size - 1 > required_size + sizeof(int)) && ((char*)current + sizeof(int) + required_size + sizeof(block) < start->end)) {
+			/*Prva moznost je ze sme nasli blok dost velky na to aby sme ho mohli rozdelit a zobrat si iba cast z neho.
+			Podmienka zaroven kontoluje aj to, ze ci je blok dost velky na to, aby,ked ho rozdelime, bolo miesto aj pre hlavicku noveho volneho bloku, ktory vznikne za obsadenym*/
 
-				struct block* novy;
-				novy = (char*)aktualny + required_size + sizeof(int);
-				novy->size = aktualny->size - required_size - sizeof(int);
-				aktualny->size = required_size;
-				novy->next = aktualny->next;
-			
-				if (start->first_free == aktualny) {
-					start->first_free = novy;
-				}
-				if (prev != NULL) {
-					prev->next = novy;
-				}
-	
-				return (char*)aktualny + sizeof(int);
+			block* new_free;
+			new_free = (char*)current + required_size + sizeof(int);
+			new_free->size = current->size - required_size - sizeof(int);
+			current->size = required_size;
+			new_free->next = current->next;
+
+			if (start->first_free == current) {
+				start->first_free = new_free;
 			}
-			else if (aktualny->size - 1 >= required_size) {					
-				/*nasli sme blok ktory obsadime cely*/
-				//printf("presne tolko miesta co treba\n\n\n");	
-				aktualny->size = aktualny->size - 1;
-				if (start->first_free == aktualny) {
-					start->first_free = aktualny->next;
-				}
-				if (prev != NULL) {
-					prev->next = aktualny->next;
-				}
-				return (char*)aktualny + sizeof(int);
+			if (prev != NULL) {
+				prev->next = new_free;
 			}
-			else {
-				//printf("nasiel sa blok velkosti %d, co je prilis malo\n", aktualny->size - 1);
+
+			return (char*)current + sizeof(int);
 			}
-			prev = aktualny;
+		else if (current->size - 1 >= required_size) {
+			/*Druha moznost je, ze sme nasli blok rovnaky alebo iba o trochu vacsi ako je pozadovana velksot,takze ho obsadime cely*/
+			current->size = current->size - 1;
+			if (start->first_free == current) {
+				start->first_free = current->next;
+			}
+			if (prev != NULL) {
+				prev->next = current->next;
+			}
+			return (char*)current + sizeof(int);
 		}
-		aktualny = aktualny->next;
+		/* ak bol volny blok mensi ako nasa pozadovana velkost, pokracujeme v hladani*/
+		prev = current;
+		current = current->next;
 	}
-	//printf("nie je miesto\n");
+	/*Ak neexistuje volny blok co by vyhovoval, vratime NULL*/
 	return NULL;
 }
 
 int memory_check(void* pointer) {
-	//printf("spusta sa funkcia memory check\n");
-	if (pointer <= start || pointer > start->end) {
-		//printf("smernik nie je z vyhradeneho pola pamate\n");
+	if (pointer <= start || pointer > start->end) {		//smernik je validny ak sa nachadza medzi smernikom na zaciatok pamate a smernikom na koniec pamate
 		return 0;
 	}
-	//printf("smernik je ok\n");
 	return 1;
 }
 
-int memory_free(void* pointer) {			
-	//printf("pustila sa funkcia memory free\n");
+int memory_free(void* pointer) {
 	if (memory_check(pointer) == 0) {
 		return 1;
 	}
-	//printf("pustila sa funkcia memory free\n");
-	struct block* novy, * aktualny;
-	novy = (char*)pointer - sizeof(int);
-	novy->size += 1;
-	/* ulozime uvolneny blok do zoznamu podla adresy */
-	if (novy < start->first_free || start->first_free == NULL) {
-		novy->next = start->first_free;
-		start->first_free = novy;
+	block* new_free, * current, * prev = NULL;
+	new_free = (char*)pointer - sizeof(int);
+	new_free->size += 1;
+	/* ulozime uvolneny blok do zoznamu podla adresy v pamati. Takto bude zoznam vzdy usporiadany podla poradia v akom su bloky v pamati ulozene */
+	if (new_free < start->first_free || start->first_free == NULL) {
+		new_free->next = start->first_free;
+		start->first_free = new_free;
 	}
 	else {
-		aktualny = start->first_free;
-		if (aktualny->next == NULL) {
-			novy->next = aktualny->next;
-			aktualny->next = novy;
+		current = start->first_free;
+		if (current->next == NULL) {
+			if (new_free > current) {
+				current->next = new_free;
+				new_free->next = NULL;
+				prev = current;
+			}
+			else {
+				start->first_free = new_free;
+				new_free->next = current;
+			}
 		}
-		else{
-			while (aktualny->next != NULL) {
-				if (novy > aktualny && novy < aktualny->next) {
-					novy->next = aktualny->next;
-					aktualny->next = novy;
+		else {
+			while (current->next != NULL) {
+				if (new_free > current && new_free < current->next) {
+					new_free->next = current->next;
+					current->next = new_free;
+					prev = current;
 					break;
 				}
-				aktualny = aktualny->next;
+				current = current->next;
 			}
 		}
 	}
-	/*tato cast spaja volne bloky co su vedla seba*/
-	aktualny = start->first_free;
-	while (aktualny->next) {
-		if (aktualny->next->size % 2 == 0) {
-			aktualny->next = NULL;
-		}
-		//printf("%d\n",aktualny->next);
-		else if (aktualny->next == (char*)aktualny + aktualny->size + sizeof(int) - 1) {
-			//printf("merge\n");
-			aktualny->size += aktualny->next->size + sizeof(int) - 1;
-			aktualny->next = aktualny->next->next;
-		}
-		else {
-			aktualny = aktualny->next;
-			//printf("e\n");
-		}
+	/*Ak sa uvolneny blok nachadza vedla ineho volneho bloku, spoji sa s nim*/
+	if (new_free->next != NULL && (new_free->next == (char*)new_free + new_free->size + sizeof(int) - 1)){		//spoji sa s nasledujucim
+		new_free->size += new_free->next->size + sizeof(int) - 1;
+		new_free->next = new_free->next->next;
 	}
-	pointer = NULL;
+	if (prev != NULL && (prev->next == (char*)prev + prev->size + sizeof(int) - 1)){			//spoji sa s predchadzajucim		
+	prev->size += prev->next->size + sizeof(int) - 1;
+	prev->next = prev->next->next;
+	}
 	return 0;
 }
-/*
-int main()
-{
-	int N = 100;
-	char region[100];
-	memory_init(region, N);
-
-	for (int i = 0; i < N; i++) {
-		printf("%d\n", region[i]);
-	}
-	//memory_check(start);
-
-	int* pointer1 = (int*)memory_alloc(3*sizeof(int));
-	for (int i = 0; i < 3; i++) {
-		pointer1[i] = 99;
-	}
-	
-	char* pointer2 = (char*)memory_alloc(22);
-	memset(pointer2, 77, 22);
-
-	for (int i = 0; i < N; i++) {
-		printf("%d\n", region[i]);
-	}
-	memory_free(pointer1);
-
-	for (int i = 0; i < N; i++) {
-		printf("%d\n", region[i]);
-	}
-
-	memory_free(pointer2);
-	memory_free(pointer2);
-
-	for (int i = 0; i < N; i++) {
-		printf("%d\n", region[i]);
-	}
-
-	char* pointer3 = (char*)memory_alloc(32);
-	memset(pointer3, 98, 32);
-
-	for (int i = 0; i < N; i++) {
-		printf("%d\n", region[i]);
-	}
-
-	memory_free(pointer2);
-
-	for (int i = 0; i < N; i++) {
-		printf("%d\n", region[i]);
-	}
-	
-
-	/*
-	char* pointer = (char*)memory_alloc(10);
-	if (pointer)
-		memset(pointer, 0, 10);
-	if (pointer)
-		memory_free(pointer);*/
-	//return 0;
-//}*/
